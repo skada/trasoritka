f2ced207ae8ea75fe85b33d60094551b4e21eb55
Jakub Skaryd <skaryd@gmail.com>
2017-05-26 15:50:42 +0200

    Sending email

--- tr/base/views.py
+++ tr/base/views.py
@@ -1,5 +1,8 @@
 import json
+
+from django.core.mail import EmailMultiAlternatives
 from django.http import Http404, HttpResponse, JsonResponse
+from django.template import Template, Context, loader
 
 
 def contact_form_post(request):
@@ -10,7 +13,23 @@ def contact_form_post(request):
     if request.user.is_anonymous():
         raise Http404()
 
-    data = request.POST
+    data = dict(request.POST)
+
+    plain_template = loader.get_template('mail/contact_mail.txt')
+    plain_message = plain_template.render(Context(data))
+
+    html_template = loader.get_template('mail/contact_mail.html')
+    html_message = html_template.render(Context(data))
+
+    msg = EmailMultiAlternatives(
+        "[ALUMNI WEB] message",
+        plain_message,
+        'noreply@absolventicvut.cz',
+        ['jakub@e-smile.cz', 'lukas@e-smile.cz',]
+    )
+    msg.attach_alternative(html_message, "text/html")
+
+    msg.send()
 
     return JsonResponse({
         'success': True
